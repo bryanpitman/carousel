7f968d6869f9fe7c672b364a7001c2e09871fcd8
Alex <alexjooho@gmail.com>
2022-08-30 17:13:04 -0700

    got code review

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -2,7 +2,7 @@ import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 import TEST_IMAGES from "./_testCommon.js";
 
-it("works when you click on arrows", function() {
+it("works when you click on right arrows", function () {
   const { container, debug } = render(
     <Carousel
       photos={TEST_IMAGES}
@@ -13,7 +13,6 @@ it("works when you click on arrows", function() {
   expect(
     container.querySelector('img[alt="testing image 1"]')
   ).toBeInTheDocument();
-  expect(container.querySelector(".bi-arrow-left-circle")).not.toBeInTheDocument();
   expect(
     container.querySelector('img[alt="testing image 2"]')
   ).not.toBeInTheDocument();
@@ -23,28 +22,49 @@ it("works when you click on arrows", function() {
   fireEvent.click(rightArrow);
   // why does img[have these brackets]????
 
+  debug()
   // expect the second image to show, but not the first
   expect(
     container.querySelector('img[alt="testing image 1"]')
   ).not.toBeInTheDocument();
-  debug()
   expect(
     container.querySelector('img[alt="testing image 2"]')
   ).toBeInTheDocument();
+});
+
+it("works when you click on left arrows", function () {
+  const { container, debug } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
 
   const leftArrow = container.querySelector(".bi-arrow-left-circle");
   fireEvent.click(leftArrow);
   expect(
     container.querySelector('img[alt="testing image 2"]')
   ).not.toBeInTheDocument();
-  debug()
   expect(
     container.querySelector('img[alt="testing image 1"]')
   ).toBeInTheDocument();
+});
 
+it("does not show left arrow on first image or right arrow on last image", function () {
+  const { container, debug } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  
+  expect(container.querySelector(".bi-arrow-left-circle")).not.toBeInTheDocument();
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
   fireEvent.click(rightArrow);
   fireEvent.click(rightArrow);
-  debug()
   expect(container.querySelector(".bi-arrow-right-circle")).not.toBeInTheDocument();
 });
 
